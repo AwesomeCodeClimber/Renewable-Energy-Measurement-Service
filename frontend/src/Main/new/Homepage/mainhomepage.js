4d09e30da62f1481be4ec27468d7276d5a6175ed
Awesome CodeClimber <strongpheonix102@gmail.com>
2021-12-28 19:18:42 +0300

    configure homepage

--- frontend/src/Main/new/Homepage/mainhomepage.js
+++ frontend/src/Main/new/Homepage/mainhomepage.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import OfferSection from '../Public/offersection'
+import Cloud from '../Public/cloud'
+import { Container } from 'react-bootstrap'
 
 export default function MainHomepage() {
     return (
-        <>
-        </>
+        <div className="homepage">
+            <Container>
+
+            </Container>
+
+            <Cloud />
+
+            <OfferSection />
+        </div>
     )
 }
