ff1ac638039dc968ebeb16f61f3e85d12371b8ae
Awesome CodeClimber <strongpheonix102@gmail.com>
2021-12-28 16:01:08 +0300

    cloud section basic addition

--- frontend/src/Main/new/Public/cloud.js
+++ frontend/src/Main/new/Public/cloud.js
@@ -1,8 +1,25 @@
-
+import * as React from 'react'
+import positive_1 from '../../../assets/img/cloud/designed/positive_0_0.png'
+import positive_2 from '../../../assets/img/cloud/designed/positive_1_0.png'
+import positive_3 from '../../../assets/img/cloud/designed/positive_2_0.png'
+import negative_1 from '../../../assets/img/cloud/designed/negative_0_0.png'
+import negative_2 from '../../../assets/img/cloud/designed/negative_1_0.png'
+import negative_3 from '../../../assets/img/cloud/designed/negative_2_0.png'
 
 export default function Cloud() {
-    return(
-        <>
-        </>
+    return (
+        <div className="cloud-section">
+            <div className="positive">
+                <img className="cloud positive-cloud positive_1" alt="positive_1" src={positive_1} />
+                <img className="cloud positive-cloud positive_2" alt="positive_2" src={positive_2} />
+                <img className="cloud positive-cloud positive_3" alt="positive_3" src={positive_3} />
+            </div>
+
+            {/* <div className="negative">
+                <img className="cloud negative-cloud" alt="negative_1" src={negative_1} />
+                <img className="cloud negative-cloud" alt="negative_2" src={negative_2} />
+                <img className="cloud negative-cloud" alt="negative_3" src={negative_3} />
+            </div> */}
+        </div>
     )
 }
